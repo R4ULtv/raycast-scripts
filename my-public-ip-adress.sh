8c88f9ad3dd8d57984c2130133099c1527cc14a0
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2025-12-24 10:37:09 +0100

    Add Get Public IP script and network icon
    - Introduced my-public-ip-adress.sh script to retrieve and copy the public IP address to the clipboard.
    - Added network-icon.png for use in Raycast interface.

---
+++ my-public-ip-adress.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Get Public IP
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon ./assets/network-icon.png
+# @raycast.packageName Network Utils
+
+# Documentation:
+# @raycast.description Get the public IP address
+# @raycast.author lil-poop
+# @raycast.authorURL https://raycast.com/lil-poop
+
+IP=$(dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com | tr -d '"')
+echo -n "$IP" | pbcopy
+echo "Copied to clipboard: $IP"
